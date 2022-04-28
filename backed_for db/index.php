915f202436d295aff7fc2ff7876f3c2fbafc2f81
abaysevi <abaysevi012@gmail.com>
2022-04-28 09:47:16 +0530

    some changes made

--- backed_for db/index.php
+++ backed_for db/index.php
@@ -1,42 +1,69 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Data collectd</title>
 
     <style>
         input[type=submit] {
-    background-color: #6E458C;
-    color: white;
-    padding: 12px 20px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-}
+            background-color: #3E5F8A;
+            color: white;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
 
-input[type=submit]:hover {
-  background-color: #45a049;
-}
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
+
+        #student {
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #student td,
+        #student th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        #student tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        #student tr:hover {
+            background-color: #ddd;
+        }
+
+        #student th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #0094ab;
+            color: white;
+        }
     </style>
 
 </head>
+
 <body>
 
-<form method="post">
-    <!-- <br> -->
-        <input type="submit" name="view_mails"
-                value="View Emails"/>
-    
-        <input type="submit" name="view_data"
-                value="View Collected Data"/>
-    <br><br>
-          
+    <form method="post">
+        <!-- <br> -->
+        <input type="submit" name="view_mails" value="View Emails" />
+
+        <input type="submit" name="view_data" value="View Collected Data" />
+        <br><br>
+
     </form>
 </body>
+
 </html>
 
 
@@ -46,8 +73,8 @@ if(!empty($_POST["view_mails"])) {
     $result= mysqli_query($conn,"SELECT * FROM registerd_users;");
     if ($result->num_rows > 0) {
         // output data of each row
-        echo "<table>"; // start a table tag in the HTML
-
+        echo "<table id=\"student\">"; // start a table tag in the HTML
+        echo"<tr><th>". "Email"."</th></tr>";
         while($row = $result->fetch_assoc()) {
             echo "<tr><td>" . htmlspecialchars($row['email']) ."</td></tr>";  //$row['index'] the index here is a field name
 
@@ -63,11 +90,19 @@ if(!empty($_POST["view_data"])) {
     $result= mysqli_query($conn,"SELECT * FROM info_colll;");
     if ($result->num_rows > 0) {
         // output data of each row
-        echo "<table>"; 
-        echo "<tr><td>" . "<strong>NAME</strong>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp". "</td><td>" . "<strong>PCM MARK</strong>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"."</td><td>" . "<strong>Admission Type</strong>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" ."</td><td>" . "<strong>Prefered Branch</strong>". "</td></tr>";  //$row['index'] the index here is a field name
+        echo "<table id=\"student\">"; 
+        echo "<tr><th>" . 
+        "<strong>NAME</strong>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp". 
+        "</th><th>" . "<strong>PCM MARK</strong>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp".
+        "</th><th>" . "<strong>Admission Type</strong>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" .
+        "</th><th>" . "<strong>Prefered Branch</strong>". "</th></tr>";  
 
         while($row = $result->fetch_assoc()) {
-            echo "<tr><td>" . htmlspecialchars($row['name']) . "</td><td>" . htmlspecialchars($row['PCM_percentage']) ."</td><td>" . htmlspecialchars($row['admi_type']) ."</td><td>" . htmlspecialchars($row['prefered_batch']) . "</td></tr>";
+            echo "<tr><td>" . htmlspecialchars($row['name']) .
+             "</td><td>" . htmlspecialchars($row['PCM_percentage']) .
+             "</td><td>" . htmlspecialchars($row['admi_type']) .
+             "</td><td>" . htmlspecialchars($row['prefered_batch']) . 
+             "</td></tr>";
              
         }
 
